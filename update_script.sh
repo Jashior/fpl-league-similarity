9f2e9520abec30e0191189820105af9b2f169de3
SaintJuniper <42616071+Jashior@users.noreply.github.com>
2024-10-20 15:19:35 +0100

    update update_script

--- update_script.sh
+++ update_script.sh
@@ -3,11 +3,20 @@
 # Change to the project directory
 cd /home/dev/fpl-league-similarity
 
-# Activate virtual environment and run analysis script
-./venv/bin/python analysis.py
+# Activate virtual environment
+source venv/bin/activate
+
+# Update requirements (uncomment if needed)
+# pip install -r requirements.txt
+
+# Run analysis script
+python analysis.py >> analysis.log 2>&1
 
 # Rebuild Angular project
-ng build --prod
+ng build --prod >> analysis.log 2>&1
 
 # Restart PM2 process
-pm2 restart fpl-league-similarity
+pm2 restart fpl-league-similarity >> analysis.log 2>&1
+
+# Deactivate virtual environment
+deactivate
