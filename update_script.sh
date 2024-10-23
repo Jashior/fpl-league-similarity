d068dc8e823b8a9bf9a5d2734c3365df59b18ea1
SaintJuniper <42616071+Jashior@users.noreply.github.com>
2024-10-23 14:59:32 +0100

    fix build script

--- update_script.sh
+++ update_script.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-
-# Change to the project directory
 cd /home/dev/fpl-league-similarity
-
-# Activate virtual environment
 source venv/bin/activate
 
-# Update requirements (uncomment if needed)
-# pip install -r requirements.txt
-
-# Run analysis script
+# Generate new data files
 python analysis.py >> analysis.log 2>&1
 
-# Rebuild Angular project
-ng build --prod >> analysis.log 2>&1
+# Rebuild Angular with new data
+ng build >> analysis.log 2>&1
 
-# Restart PM2 process
-pm2 restart fpl-league-similarity >> analysis.log 2>&1
+# Reload will gracefully restart and pick up the new build
+pm2 reload fpl-league-similarity >> analysis.log 2>&1
 
-# Deactivate virtual environment
 deactivate
